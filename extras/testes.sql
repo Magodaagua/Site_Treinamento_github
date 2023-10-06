0cd0e6ae7ab85b739a2d249c36c30d34af33bfb4
vsofiato <vsofiato@copimaq.local>
2023-10-06 17:58:22 -0300

    banco de dados atualizado

--- extras/testes.sql
+++ extras/testes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 15/09/2023 às 22:14
+-- Tempo de geração: 06/10/2023 às 22:56
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.2.4
 
@@ -47,22 +47,23 @@ INSERT INTO `administrador` (`ID_admin`, `email`, `senha`) VALUES
 --
 
 CREATE TABLE `categoria` (
+  `id` int(200) NOT NULL,
   `Nome_cat` varchar(500) NOT NULL,
   `imagem` varchar(500) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `categoria`
 --
 
-INSERT INTO `categoria` (`Nome_cat`, `imagem`) VALUES
-('Administração', '0'),
-('Atendimento', '0'),
-('Contabilidade', '0'),
-('Informática', '0'),
-('Marketing', '0'),
-('Mecânica', '0'),
-('Vendas', '0');
+INSERT INTO `categoria` (`id`, `Nome_cat`, `imagem`) VALUES
+(1, 'Administração', 'Administração.png'),
+(2, 'Atendimento', 'Atendimento.png'),
+(3, 'Contabilidade', 'Contabilidade.png'),
+(4, 'Informática', 'Informática.png'),
+(5, 'Marketing', 'Marketing.png'),
+(6, 'Mecânica', 'Mecânica.png'),
+(7, 'Vendas', 'Vendas.png');
 
 -- --------------------------------------------------------
 
@@ -77,25 +78,26 @@ CREATE TABLE `curso` (
   `Subcategoria` varchar(400) NOT NULL,
   `Descricao` longtext NOT NULL,
   `Datadecriacao` date NOT NULL,
-  `imagem` varchar(500) NOT NULL
+  `imagem` varchar(500) NOT NULL,
+  `pdf` varchar(500) NOT NULL,
+  `video` varchar(500) NOT NULL,
+  `prova` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Despejando dados para a tabela `curso`
 --
 
-INSERT INTO `curso` (`ID_curso`, `Nome`, `Categoria`, `Subcategoria`, `Descricao`, `Datadecriacao`, `imagem`) VALUES
-(1, 'Linguagem C', 'Informática', 'Programação', 'Curso para ensinar C básico', '2023-08-20', 'csharp.png'),
-(2, 'Python', 'Informática', 'Programação', 'Curso para ensinar Python básico', '2022-12-12', '2.png'),
-(3, 'PacoteOffice', 'Informática', 'Office', 'Curso para ensinar Pacote Office avançado', '2023-08-31', '3.png'),
-(4, 'C#', 'Informática', 'Programação Orientada a Objeto', 'Curso para ensinar C# intermediário', '2023-08-18', '4.png'),
-(5, 'Administração de Empresas', 'Administração', 'Empresas', 'Curso para ensinar Administração básico', '2023-02-27', '5.png'),
-(6, 'Java', 'Informática', 'Programação Orientada a Objeto', 'Curso para ensinar Java básico', '0000-00-00', '6.png'),
-(7, 'Contabilidade', 'Contabilidade', 'Empresas', 'Curso para ensinar Administração básico', '2023-02-27', '7.png'),
-(8, 'PHP', 'Informática', 'Programação Orientada a Objeto', 'Curso para ensinar Java básico', '2021-04-08', '8.png'),
-(10, 'teste', 'Informática', 'Programação Orientada a Objeto', 'Unity é uma ferramenta que permite criar videojogos para diversas plataformas (PC, consolas, mobile, VR e AR) utilizando um editor visual e programação através de scripting,', '2023-09-21', ''),
-(12, 'teste4', 'Informática', 'sadasd', 'O NDD Print é uma solução internacional voltada para o mercado de impressão, com tecnologias para provedores de outsourcing de impressão e clientes desses provedores.', '2023-09-01', 'papelaria.png'),
-(26, 'Victor', 'Informática', 'sadasd', 'Curso para ensinar Java básico', '2023-10-01', '20180711075154_7085992915_D.png');
+INSERT INTO `curso` (`ID_curso`, `Nome`, `Categoria`, `Subcategoria`, `Descricao`, `Datadecriacao`, `imagem`, `pdf`, `video`, `prova`) VALUES
+(1, 'Linguagem C', 'Informática', 'Programação', 'Curso para ensinar C básico', '2023-08-20', 'csharp.png', '', '', ''),
+(2, 'Python', 'Informática', 'Programação', 'Curso para ensinar Python básico', '2022-12-12', '2.png', '2.pdf', '2.mp4', ''),
+(3, 'PacoteOffice', 'Informática', 'Office', 'Curso para ensinar Pacote Office avançado', '2023-08-31', '3.png', '', '', ''),
+(4, 'C#', 'Informática', 'Programação Orientada a Objeto', 'Curso para ensinar C# intermediário', '2023-08-18', '4.png', '', '', ''),
+(5, 'Administração de Empresas', 'Administração', 'Empresas', 'Curso para ensinar Administração básico', '2023-02-27', '5.png', '', '', ''),
+(6, 'Java', 'Informática', 'Programação Orientada a Objeto', 'Curso para ensinar Java básico', '0000-00-00', '6.png', '', '', ''),
+(7, 'Contabilidade', 'Contabilidade', 'Empresas', 'Curso para ensinar Administração básico', '2023-02-27', '7.png', '', '', ''),
+(8, 'PHP', 'Informática', 'Programação Orientada a Objeto', 'Curso para ensinar Java básico', '2021-04-08', '8.png', '', '', ''),
+(10, 'teste', 'Informática', 'Programação Orientada a Objeto', 'Unity é uma ferramenta que permite criar videojogos para diversas plataformas (PC, consolas, mobile, VR e AR) utilizando um editor visual e programação através de scripting,', '2023-09-21', '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -110,15 +112,21 @@ CREATE TABLE `menu` (
   `texto3` text NOT NULL,
   `titulo1` varchar(500) NOT NULL,
   `titulo2` varchar(500) NOT NULL,
-  `titulo3` varchar(500) NOT NULL
+  `titulo3` varchar(500) NOT NULL,
+  `imagem1` varchar(500) NOT NULL,
+  `imagem2` varchar(500) NOT NULL,
+  `imagem3` varchar(500) NOT NULL,
+  `carrosel1` varchar(500) NOT NULL,
+  `carrosel2` varchar(500) NOT NULL,
+  `carrosel3` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Despejando dados para a tabela `menu`
 --
 
-INSERT INTO `menu` (`ID_menu`, `texto1`, `texto2`, `texto3`, `titulo1`, `titulo2`, `titulo3`) VALUES
-(1, 'Some great placeholder content for the first featurette here. Imagine some exciting prose here.', 'Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.', 'And yes, this is the last block of representative placeholder content. Again, not really intended to be actually read, simply here to give you a better view of what this would look like with some actual content. Your content.\r\n\r\n', 'First featurette heading. It’ll blow your mind.', 'Oh yeah, it’s that good. See for yourself.', 'And lastly, this one. Checkmate.');
+INSERT INTO `menu` (`ID_menu`, `texto1`, `texto2`, `texto3`, `titulo1`, `titulo2`, `titulo3`, `imagem1`, `imagem2`, `imagem3`, `carrosel1`, `carrosel2`, `carrosel3`) VALUES
+(1, 'Some great placeholder content for the first featurette here. Imagine some exciting prose here.', 'Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.', 'And yes, this is the last block of representative placeholder content. Again, not really intended to be actually read, simply here to give you a better view of what this would look like with some actual content. Your content.', 'First featurette heading. It’ll blow your mind.', 'Oh yeah, it’s that good. See for yourself.', 'And lastly, this one. Checkmate.', 'carrosel.png', 'carrosel2.jpeg', 'carrosel3.jpg', 'carrosel.png', 'carrosel2.jpeg', 'carrosel3.jpg');
 
 -- --------------------------------------------------------
 
@@ -146,8 +154,26 @@ INSERT INTO `parceiro` (`ID_parceiro`, `Nome`, `Descricao`, `link`, `imagem`) VA
 (5, 'SafeQ', 'O YSoft SafeQ é um conjunto de soluções modulares de gestão de impressão empresarial a nível mundial, que permite às organizações assumir o controlo do ambiente de impressão, reduzir custos, aumentar a segurança e a conformidade dos seus documentos, melhorar a produtividade no escritório e, ao mesmo tempo, minimizar o impacto ambiental da impressão.', 'https://www.ysoft.com/en/solutions/print-management', '5.png'),
 (6, 'NDDPrint', 'O NDD Print é uma solução internacional voltada para o mercado de impressão, com tecnologias para provedores de outsourcing de impressão e clientes desses provedores.', 'https://kubolms.com.br/nddacademy/', '6.png'),
 (7, 'Unity3D', 'Unity é uma ferramenta que permite criar videojogos para diversas plataformas (PC, consolas, mobile, VR e AR) utilizando um editor visual e programação através de scripting,', 'https://unity.com/pt', ''),
-(9, 'teste', 'Unity é uma ferramenta que permite criar videojogos para diversas plataformas (PC, consolas, mobile, VR e AR) utilizando um editor visual e programação através de scripting,', 'https://app.printwayy.com/Account/Login?ReturnUrl=/', ''),
-(15, 'Carlos Nascimento', 'Unity é uma ferramenta que permite criar videojogos para diversas plataformas (PC, consolas, mobile, VR e AR) utilizando um editor visual e programação através de scripting,', 'https://unity.com/pt', '20180711075154_7085992915_D.png');
+(9, 'teste', 'Unity é uma ferramenta que permite criar videojogos para diversas plataformas (PC, consolas, mobile, VR e AR) utilizando um editor visual e programação através de scripting,', 'https://app.printwayy.com/Account/Login?ReturnUrl=/', 'prova.png');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `rodape`
+--
+
+CREATE TABLE `rodape` (
+  `ID_rodape` int(2) NOT NULL,
+  `politica` varchar(500) NOT NULL,
+  `termos` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `rodape`
+--
+
+INSERT INTO `rodape` (`ID_rodape`, `politica`, `termos`) VALUES
+(1, 'Teste 1', 'Teste 2');
 
 -- --------------------------------------------------------
 
@@ -187,14 +213,13 @@ ALTER TABLE `administrador`
 -- Índices de tabela `categoria`
 --
 ALTER TABLE `categoria`
-  ADD PRIMARY KEY (`Nome_cat`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Índices de tabela `curso`
 --
 ALTER TABLE `curso`
-  ADD PRIMARY KEY (`ID_curso`),
-  ADD KEY `Categoria` (`Categoria`);
+  ADD PRIMARY KEY (`ID_curso`);
 
 --
 -- Índices de tabela `menu`
@@ -207,6 +232,12 @@ ALTER TABLE `menu`
 --
 ALTER TABLE `parceiro`
   ADD PRIMARY KEY (`ID_parceiro`);
+
+--
+-- Índices de tabela `rodape`
+--
+ALTER TABLE `rodape`
+  ADD PRIMARY KEY (`ID_rodape`);
 
 --
 -- Índices de tabela `usuario`
@@ -225,10 +256,16 @@ ALTER TABLE `administrador`
   MODIFY `ID_admin` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT de tabela `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
 -- AUTO_INCREMENT de tabela `curso`
 --
 ALTER TABLE `curso`
-  MODIFY `ID_curso` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `ID_curso` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT de tabela `menu`
@@ -243,20 +280,16 @@ ALTER TABLE `parceiro`
   MODIFY `ID_parceiro` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
+-- AUTO_INCREMENT de tabela `rodape`
+--
+ALTER TABLE `rodape`
+  MODIFY `ID_rodape` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
   MODIFY `ID_usuario` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- Restrições para tabelas despejadas
---
-
---
--- Restrições para tabelas `curso`
---
-ALTER TABLE `curso`
-  ADD CONSTRAINT `curso_ibfk_1` FOREIGN KEY (`Categoria`) REFERENCES `categoria` (`Nome_cat`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
